8dc072f5fc7572d8d927e9e31beb249e75d20029
瀬戸啓一朗 <kei@kei-mb.local>
2017-03-01 21:09:28 +0900

    edit routes for omniouthable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root 'posts#index'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :posts
   resources :users
-  #get 'users/:id' => 'users#show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
