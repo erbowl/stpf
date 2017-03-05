9ed69a06c3f95b65f8648d1eddf83ea700739eae
瀬戸啓一朗 <kei@kei-mb.local>
2017-03-06 01:43:06 +0900

    大量のパーシャる作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
     title = @page_title + " | " + title if @page_title
     title
   end
+
+  def top_button(array=[],css)
+    opt=[]
+    opt[0]=array
+    opt[1]=css
+    opt.inspect
+  render :inline => <<-HAML, :type => :haml, :locals => {:opt => opt}
+.btn-group.btn-group-justified{:role => "group"}
+  - opt[0].each do |key,path|
+    .btn-group{:role => "group"}
+      %button.btn#{opt[1]}=link_to key,path,class:"active"
+  HAML
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root 'users#index'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",
-                                      :sessions=>"users/sessions",:passwords=>"users/passwords}
+                                      :sessions=>"users/sessions",:passwords=>"users/passwords",:registrations=>"users/registrations"}
   resources :posts
   resources :users
   get 'cheers/:id',to: "users#cheer",as: "cheer"
